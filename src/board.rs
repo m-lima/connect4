e2f21fd18449bb0d6678945c4eda11009892d5af
Marcelo Lima <marcelowind@gmail.com>
2019-06-30 17:07:11 +0200

    Getting ready for compile time creation of the board

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, Copy, Clone)]
-pub enum Player {
+enum Player {
     NONE,
     ONE,
     TWO,
@@ -8,48 +8,39 @@ pub enum Player {
 impl std::fmt::Display for Player {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Player::NONE => write!(f, " "),
-            Player::ONE => write!(f, "X"),
-            Player::TWO => write!(f, "O"),
+            Player::NONE => write!(f, "  "),
+            Player::ONE => write!(f, "██"),
+            Player::TWO => write!(f, "░░"),
         }
     }
-}
-
-struct Row {
-    cells: [Player; 8],
-}
-
-impl std::fmt::Display for Row {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        for v in self.cells.iter() {
-            write!(f, "|{}", v)?;
-        }
-        write!(f, "|")
-    }
-}
-
-impl Row {
-    fn empty() -> Row{Row{ cells: [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE] }}
 }
 
 pub struct Board {
-    rows: [Row; 8],
+    cells: [[Player; 8]; 8],
 }
 
 impl std::fmt::Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        for  v in self.rows.iter() {
-            write!(f, "{}\n", v)?;
+        for row in self.cells.iter() {
+            for cell in row.iter() {
+                write!(f, "|{}", cell)?;
+            }
+            write!(f, "|\n")?;
         }
 
-        write!(f, "-----------------")
+        write!(f, "-------------------------")
     }
 }
 
 impl Board {
-    pub fn new() -> Board{Board{ rows: [Row::empty(), Row::empty(), Row::empty(), Row::empty(), Row::empty(), Row::empty(), Row::empty(), Row::empty()] }}
-
-    pub fn cell(&self, column: u8, row: u8) -> Player {
-        self.rows[row as usize].cells[column as usize]
-    }
+    pub fn new() -> Board{Board{ cells: [
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    [Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE, Player::NONE],
+    ]}}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 pub mod board;
 
 fn main() {
-    println!("Hello World!");
     let b = board::Board::new();
-    // println!("Cell: {}", b.cell(9, 3));
     println!("{}", b);
 }
 
